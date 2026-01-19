719f9d6d330579db585b0e7ed74ff48812d7f96d
PeterT800 <peto.tatransky@gmail.com>
2026-01-19 18:17:55 +0100

    Chapter 2: Variables and simple data types

---
+++ famous_quote.py
@@ -0,0 +1,1 @@
+print('Buddha once said: "If anything is worth doing, do it with all your heart."')
---
+++ famous_quote_2.py
@@ -0,0 +1,3 @@
+name = "Buddha"
+quote = "If anything is worth doing, do it with all your heart."
+print(f'{name} once said: "{quote}"')
